c40f1690b84c9ac168f5e300e443ec11824f59a6
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-08-24 08:10:26 -0400

    Initial commit; boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component and this component should produce some HTML
+const App = () => {
+	return <div>Hi!</div>;
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// take this component's generated HTML and put it into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
