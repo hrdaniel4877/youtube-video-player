b463b1ab051c7546a41a94012f77b06a3b34af7e
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-09-03 09:02:25 -0400

    Refactor functional component App to class based component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 
 const API_KEY = "AIzaSyBal0-B5ARmac3M37TRcB6eZb4EEzO8rno";
 
-YTsearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
-
 // create a new component and this component should produce some HTML
-const App = () => {
-	return ( 
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {videos: []};
+		
+		YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({videos});
+		});
+	}
+	render() {
+		return ( 
+			<div>
+				<SearchBar />
+			</div>
+		);
+	};
 };
 
 // take this component's generated HTML and put it into the DOM
