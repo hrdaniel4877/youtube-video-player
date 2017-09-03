efd30f1654743c2d85fcc8eecbeddd75dce0ca99
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-09-03 08:58:23 -0400

    Create functional App component and class based SearchBar components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar'
 
 const API_KEY = "AIzaSyBal0-B5ARmac3M37TRcB6eZb4EEzO8rno";
@@ -13,7 +12,6 @@ const App = () => {
 		</div>
 	);
 };
-
 
 // take this component's generated HTML and put it into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
