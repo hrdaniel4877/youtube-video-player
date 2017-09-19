0a6b2815aee5b6e7f5ff09cd5198b4c437848bc4
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-09-19 18:10:27 -0400

    Add HTML markup to the VideoListItem component. Also add thumbnail URL and title from YouTube API response.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,7 @@ const VideoList = (props) => {
 
 	const videoItems = props.videos.map(
 		(video) => {
-			return <VideoListItem video={video} />
+			return <VideoListItem key={video.etag} video={video} />
 		}
 	);
 
@@ -14,6 +14,7 @@ const VideoList = (props) => {
 			{videoItems}
 		</ul>
 	);
+
 };
 
 export default VideoList;
